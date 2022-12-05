5713a637054bc4b8cd66f246e28cbd4980809c75
xlsmouselet001 <110027351+xlsmouselet001@users.noreply.github.com>
2022-12-04 22:39:38 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # set val
 PORT=${PORT:-8080}
-AUUID=${AUUID:-5194845a-cacf-4515-8ea5-fa13a91b1026}
+AUUID=${AUUID:5194845a-cacf-4515-8ea5-fa13a91b1026}
 ParameterSSENCYPT=${ParameterSSENCYPT:-chacha20-ietf-poly1305}
 CADDYIndexPage=${CADDYIndexPage:-https://codeload.github.com/ripienaar/free-for-dev/zip/master}
 
